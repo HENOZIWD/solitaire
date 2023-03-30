b6e6c643804028fd54417a8afb72022e24ea091b
Donghyeon Lee <ehdgus001928@gmail.com>
2023-03-30 20:30:49 +0900

    Add stacking card on cards

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,70 +9,115 @@ export interface ICardPosition {
 
 const App = () => {
 
-  const getRandomArbitrary = (min: number, max: number) => {
-    return Math.random() * (max - min) + min;
-  }
-
   const baseFontSizeString = getComputedStyle(document.documentElement).getPropertyValue('font-size');
   const baseFontSize = +baseFontSizeString.slice(0, baseFontSizeString.indexOf('px'));
   const boardWidth = baseFontSize * 50;
   const boardHeight = baseFontSize * 30;
+  const cardWidth = baseFontSize * 5;
+  const cardHeight = baseFontSize * 5;
+  const gap = baseFontSize;
   const [cardPositions, setCardPositions] = useState<ICardPosition[]>([
-    { x: getRandomArbitrary(0, boardWidth - baseFontSize * 5), y: getRandomArbitrary(0, boardHeight - baseFontSize * 5) },
-    { x: getRandomArbitrary(0, boardWidth - baseFontSize * 5), y: getRandomArbitrary(0, boardHeight - baseFontSize * 5) },
-    { x: getRandomArbitrary(0, boardWidth - baseFontSize * 5), y: getRandomArbitrary(0, boardHeight - baseFontSize * 5) },
-    { x: getRandomArbitrary(0, boardWidth - baseFontSize * 5), y: getRandomArbitrary(0, boardHeight - baseFontSize * 5) },
-    { x: getRandomArbitrary(0, boardWidth - baseFontSize * 5), y: getRandomArbitrary(0, boardHeight - baseFontSize * 5) },
-    { x: getRandomArbitrary(0, boardWidth - baseFontSize * 5), y: getRandomArbitrary(0, boardHeight - baseFontSize * 5) },
+    { x: 0 + gap / 2, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 2, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 4, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 6, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 8, y: 0 + gap / 2 },
   ]);
-  const [stack, setStack] = useState<Array<number>>([0]);
+  const [realCardPositions, setRealCardPositions] = useState<ICardPosition[]>([
+    { x: 0 + gap / 2, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 2, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 4, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 6, y: 0 + gap / 2 },
+    { x: 0 + gap / 2 + cardWidth * 8, y: 0 + gap / 2 }, 
+  ]);
+  // const [cardStacks, setCardStacks] = useState<number[][]>(Array.from({ length: 5 }, () => []));
 
-  const pushStack = (i: number) => {
-    if (stack[stack.length - 1] + 1 === i) {
-      const newStack = stack.slice();
-      newStack.push(i);
-      setStack(newStack);
+  // const stackCard = (i: number) => {
+
+  // }
+
+  const checkCardBoundary = (i: number, pos: ICardPosition) => {
+    let newPos = {...pos};
+    let isStacked = false;
+
+
+    for (let cardIdx = 0; cardIdx < i; cardIdx++) {
+      if (cardIdx !== i &&
+        cardPositions[cardIdx].x - gap < newPos.x &&
+        newPos.x < cardPositions[cardIdx].x + gap &&
+        cardPositions[cardIdx].y + baseFontSize * 3 / 2 - gap < newPos.y &&
+        newPos.y < cardPositions[cardIdx].y + baseFontSize * 3 / 2 + gap) {
+
+          newPos = { x: cardPositions[cardIdx].x, y: cardPositions[cardIdx].y + baseFontSize * 3 / 2};
+          isStacked = true;
+          break;
+      }
     }
+
+    return { isStacked: isStacked, newPos: newPos };
   }
 
-  const popStack = (i: number) => {
-    if (stack[stack.length - 1] === i) {
-      const newStack = stack.slice();
-      newStack.pop();
-      setStack(newStack);
+  const checkBoardBoundary = (i: number, pos: ICardPosition) => {
+    let newPos = {...pos};
+    if (newPos.x < 0 + gap) {
+      newPos.x = 0 + gap / 2;
     }
+    else if (boardWidth - cardWidth - gap < newPos.x) {
+      newPos.x = boardWidth - cardWidth - gap / 2;
+    }
+
+    if (newPos.y < 0 + gap) {
+      newPos.y = 0 + gap / 2;
+    }
+    else if (boardHeight - cardHeight - gap < newPos.y) {
+      newPos.y = boardHeight - cardHeight - gap / 2;
+    }
+
+    return newPos;
   }
 
-  const movePosition = (i: number, x: number, y: number) => {
+  const checkBoundary = (i: number, pos: ICardPosition) => {
+    let newPos = pos;
+
+    newPos = checkBoardBoundary(i, newPos);
+    newPos = checkCardBoundary(i, newPos).newPos;
+
+    return newPos;
+  }
+
+  const movePosition = (i: number, newPos: ICardPosition) => {
     const newCardPositions = cardPositions.slice();
-    newCardPositions[i] = { x: x, y: y };
+    newCardPositions[i] = checkBoundary(i, newPos);
     setCardPositions(newCardPositions);
+  }
+
+  const moveRealPosition = (i: number, newPos: ICardPosition) => {
+    const newRealCardPositions = realCardPositions.slice();
+    newRealCardPositions[i] = {...newPos};
+    setRealCardPositions(newRealCardPositions);
+    setCardPositions(newRealCardPositions);
   }
 
   return (
     <>
     <div className={styles.container}>
       <div className={styles.board}>
-        <div className={styles.cell} />
         {cardPositions.map((c, i) => (
           <Card
             key={i}
             cardIndex={i}
-            xPos={cardPositions[i].x}
-            yPos={cardPositions[i].y}
+            pos={cardPositions[i]}
+            realPos={realCardPositions[i]}
             boardWidth={boardWidth}
             boardHeight={boardHeight}
+            cardWidth={cardWidth}
+            cardHeight={cardHeight}
             movePosition={movePosition}
-            pushStack={pushStack}
-            popStack={popStack}
+            moveRealPosition={moveRealPosition}
+            checkCardBoundary={checkCardBoundary}
           />
         ))}
       </div>
-      {stack.map((num, index) => (
-        <div key={index}>
-          {num}
-        </div>
-      ))}
     </div>
     </>
   );
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../styles/Card.module.css';
+import { ICardPosition } from './App';
 
 interface ICardProps {
   cardIndex: number;
-  xPos: number;
-  yPos: number;
+  pos: ICardPosition;
+  realPos: ICardPosition;
   boardWidth: number;
   boardHeight: number;
-  movePosition: (i: number, x: number, y: number) => void;
-  pushStack: (i: number) => void;
-  popStack: (i: number) => void;
+  cardWidth: number;
+  cardHeight: number;
+  movePosition: (i: number, newPos: ICardPosition) => void;
+  moveRealPosition: (i: number, newPos: ICardPosition) => void;
+  checkCardBoundary: (i: number, pos: ICardPosition) => { isStacked: boolean, newPos: ICardPosition }
 }
 
 const Card = (props: ICardProps) => {
 
-  const baseFontSizeString = getComputedStyle(document.documentElement).getPropertyValue('font-size');
-  const baseFontSize = +baseFontSizeString.slice(0, baseFontSizeString.indexOf('px'));
-  const cardWidth = baseFontSize * 5;
-  const cardHeight = baseFontSize * 5;
-  const gap = baseFontSize * 2;
-
   const [clicked, setClicked] = useState<boolean>(false);
 
   useEffect(() => {
-    if (!clicked && props.xPos === baseFontSize * 23 && props.yPos === baseFontSize * 11.5) {
-      props.pushStack(props.cardIndex + 1);
+    if (!clicked) {
+      const moveResult = props.checkCardBoundary(props.cardIndex, props.pos);
+      if (moveResult.isStacked) {
+        props.moveRealPosition(props.cardIndex, moveResult.newPos);
+      }
+      else {
+        props.movePosition(props.cardIndex, props.realPos);
+      }
     }
-    else if (!clicked && !(props.xPos === baseFontSize * 23 && props.yPos === baseFontSize * 11.5)){
-      props.popStack(props.cardIndex + 1);
-    }
-  }, [clicked]);
+  }, [clicked])
 
   const mouseDown = (clickEvent: React.MouseEvent<HTMLDivElement>) => {
     clickEvent.preventDefault();
@@ -38,30 +38,7 @@ const Card = (props: ICardProps) => {
       const deltaX = moveEvent.clientX - clickEvent.clientX;
       const deltaY = moveEvent.clientY - clickEvent.clientY;
 
-      let nextX = props.xPos + deltaX;
-      let nextY = props.yPos + deltaY;
-
-      if (nextX < 0 + gap) {
-        nextX = 0 + gap / 4;
-      }
-      if (props.boardWidth - cardWidth - gap < nextX) {
-        nextX = props.boardWidth - cardWidth - gap / 4;
-      }
-
-      if (nextY < 0 + gap) {
-        nextY = 0 + gap / 4;
-      }
-      if (props.boardHeight - cardHeight - gap < nextY) {
-        nextY = props.boardHeight - cardHeight - gap / 4;
-      }
-
-      if (baseFontSize * 22.5 - gap / 2 < nextX && nextX < baseFontSize * 22.5 + gap &&
-          baseFontSize * 11 - gap / 2 < nextY && nextY < baseFontSize * 11 + gap) {
-        nextX = baseFontSize * 23;
-        nextY = baseFontSize * 11.5;
-      }
-
-      props.movePosition(props.cardIndex, nextX, nextY);
+      props.movePosition(props.cardIndex,{ x: props.pos.x + deltaX, y: props.pos.y + deltaY });
     }
 
     const mouseUp = () => {
@@ -80,9 +57,9 @@ const Card = (props: ICardProps) => {
       className={styles.card}
       onMouseDown={mouseDown}
       style={{ 
-        transform: `translateX(${props.xPos}px) translateY(${props.yPos}px) scale(${clicked ? 0.9 : 1})`,
-        width: `${cardWidth}px`,
-        height: `${cardHeight}px`,
+        transform: `translateX(${props.pos.x}px) translateY(${props.pos.y}px) scale(${clicked ? 0.9 : 1})`,
+        width: `${props.cardWidth}px`,
+        height: `${props.cardHeight}px`,
       }}
     >
       {props.cardIndex + 1}
